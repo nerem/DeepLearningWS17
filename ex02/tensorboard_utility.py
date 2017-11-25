90bd359d8c554f98d47aacefddf42adf8a05252b
Felix <felix.kammerlander@gmail.com>
2017-11-25 12:12:57 +0100

    added tensorboard paths for my desktop pc
    war natürlich leider doch nur für mich als user installiert...

--- ex02/tensorboard_utility.py
+++ ex02/tensorboard_utility.py
@@ -6,6 +6,8 @@ import webbrowser
 
 python_path = "C:\\ProgramData\\Anaconda3\\envs\\tensorflow\\python.exe"
 tensorboard_path = "C:\\ProgramData\\Anaconda3\\envs\\tensorflow\\Lib\\site-packages\\tensorboard\\main.py"
+python_path_felix = "C:\\Users\\Felix\\AppData\\Local\\conda\\conda\\envs\\tensorflow\\python.exe"
+tensorboard_path_felix = "C:\\Users\\Felix\\AppData\\Local\\conda\\conda\\envs\\tensorflow\\Lib\\site-packages\\tensorboard\\main.py"
 
 def launch_tb(sub_dir = None):
     if sub_dir is None:
@@ -17,6 +19,24 @@ def launch_tb(sub_dir = None):
         return
     def tb():
         os.system("{} {} --logdir={}".format(python_path, tensorboard_path, log_dir))
+        return
+    t = threading.Thread(target = tb, args=([]))
+    t.daemon = True
+    t.start()
+    sleep(0.5)
+    webbrowser.open("http:\\localhost:6006")
+    return
+
+def launch_tb_felix(sub_dir = None):
+    if sub_dir is None:
+        log_dir = os.getcwd() + "\\logs"
+    else:
+        log_dir = os.getcwd() + "\\logs" + "\\" + sub_dir
+    if not os.path.isdir(log_dir):
+        print("No logs.")
+        return
+    def tb():
+        os.system("{} {} --logdir={}".format(python_path_felix, tensorboard_path_felix, log_dir))
         return
     t = threading.Thread(target = tb, args=([]))
     t.daemon = True
